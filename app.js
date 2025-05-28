0105296eafddb0a895769b9f738b71cc1fb7d93b
Ldh <91560378+LucasDerhore@users.noreply.github.com>
2025-05-28 13:48:14 +0200

    mocked tasks to test

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tasks = [
+  {
+    title: 'Learn to code',
+    done: false,
+  },
+  {
+    title: 'Test',
+    done: true,
+  }
+];
+
+console.log(tasks);
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
